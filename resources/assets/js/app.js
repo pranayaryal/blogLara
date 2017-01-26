5850f406307b4c1298933c8aeeb69a38506e2704
Jerrod Long <jlong@doeanderson.com>
2017-01-26 16:34:09 -0500

    Basic components for posts and post created. Add content to posts table

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,8 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('posts', require('./components/Posts.vue'));
+Vue.component('admin', require('./components/Admin.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/posts/Posts.vue
@@ -0,0 +1,32 @@
+<style scoped>
+
+</style>
+
+<template>
+    <div class="container">
+        <post v-for="post in posts" >
+    </div>
+</template>
+
+<script>
+import Post from './components/posts/Post.vue';
+function postsInitialState() {
+    return {
+        post: {
+            title: '',
+            featured_image: '',
+            content: '',
+            created_at: ''
+        }
+    }
+}
+export default {
+    data() {
+        this.posts = postsInitialState()
+    },
+    props: {
+        post: Post
+    }
+}
+
+</script>
