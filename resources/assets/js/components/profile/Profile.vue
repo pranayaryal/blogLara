3284f33e4d05e49aa0ec6fbb2ad10ed02d9c2d08
Zach Hornsby <me@zachhornsby.com>
2017-02-03 16:21:47 -0500

    [change] profile styling

--- resources/assets/js/components/profile/Profile.vue
+++ resources/assets/js/components/profile/Profile.vue
@@ -3,22 +3,26 @@
 </style>
 
 <template>
-    <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <img :src="profile.avatar" alt="Profile of ...">
-            </div>
+    <div class="shell">
+		<div class="user">
+			<div class="user_details">
+				<div class="user_avatar" :style="{ 'background-image': 'url(' + profile.avatar + ')' }"></div>
+				<h2 class="user_name">Zach Hornsby</h2>
+				<p class="user_title">Senior Font End Developer</p>
 
-            <div class="panel-body">
-                <p>{{ profile.bio }} </p>
-                <ul>
-                    <li><a :href="profile.instagram">Instagram</a></li>
-                    <li><a :href="profile.twitter">Twitter</a></li>
-                </ul>
-            </div>
-        </div>
+				<ul class="user_social">
+					<li><a :href="profile.instagram"><i class="icon-instagram"></i></a></li>
+					<li><a :href="profile.twitter"><i class="icon-twitter"></i></a></li>
+					<li><a :href="profile.github"><i class="icon-github"></i></a></li>
+					<li><a :href="profile.dribbble"><i class="icon-dribbble"></i></a></li>
+				</ul>
+			</div>
+			
+			<div class="user_bio">
+				{{ profile.bio }}
+			</div>
+		</div>
     </div>
-
 </template>
 
 <script>
