f8cd27ec85009c36cd5e40813088cb9123e033ad
Jerrod Long <jlong@doeanderson.com>
2017-02-03 16:57:46 -0500

    Add additional social fields to profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,7 +15,9 @@ class ProfileController extends Controller
             'bio' => 'I created Twitter, Facebook, and Beanie Babies all before I turned 30. You should definitely listen to everything I write and shit.',
             'avatar' => 'https://avatars2.githubusercontent.com/u/2627466?v=3&s=460',
             'twitter' => 'https://twitter.com/zachhornsby',
-            'instagram' => 'https://www.instagram.com/zachhornsby/'
+            'instagram' => 'https://www.instagram.com/zachhornsby/',
+            'dribbble' => 'https://dribbble.com/zachhornsby',
+            'github' => 'https://github.com/zachhornsby'
         ]);
     }
 
