d2b8ea8bd23d4a3640224f40a37ddc7175d3c5b0
Jerrod Long <jlong@doeanderson.com>
2017-08-29 08:59:15 -0400

    [update] Add vendor folder

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :application, "Doe-Anderson-Digital-Blog"
+set :repo_url,  "git@github.com:doeanderson/digital"
+set :deploy_to, "/opt/sites/digital.doe1915.com"
+
+set :keep_releases, 5
+
+# set :linked_dirs, %w{storage storage/app storage/framework storage/logs}
