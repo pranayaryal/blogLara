538455852c127b169ee2dd6b61774d6fd3517d7f
Jerrod Long <jlong@doeanderson.com>
2017-12-18 21:01:30 -0500

    [add] Bulma markup to profile form.

--- resources/views/profile/form.blade.php
+++ resources/views/profile/form.blade.php
@@ -2,44 +2,39 @@
 @section('content')
 
 @include('errors')
-<form class="form-horizontal" role="form" action="/profile" method="POST" enctype="multipart/form-data">
+<form class="form" role="form" action="/profile" method="POST" enctype="multipart/form-data">
     {{ csrf_field() }}
 
     @if (isset($profile->id) && !empty($profile->id))
         {{ method_field('PUT') }}
         <input type="hidden" name="user_id" value="{{ $profile->user->id }}">
     @endif
+    <div class="columns">
+        <div class="column is-one-half">
+            <div class="field">
+                <label class="label control-label" for="name">Name</label>
 
+                <div class="control">
+                    <input type="text" class="input" name="name" value="{{ $profile->user->name }}">
+                </div>
+            </div>
+        </div>
+        <div class="column is-one-half">
+            <div class="field">
+                <label class="label control-label" for="title">Title</label>
 
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="name">Name</label>
-
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="name" value="{{ $profile->user->name }}">
-
-            <span class="help-block">
-                Your name as it will appear on this site.
-            </span>
+                <div class="control">
+                    <input type="text" class="input" name="title" value="{{ $profile->title }}">
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="title">Title</label>
+    <div class="field">
+        <label class="label control-label" for="avatar">Avatar</label>
 
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="title" value="{{ $profile->title }}">
-
-            <span class="help-block">
-                Your title here at Doe-Anderson
-            </span>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="avatar">Avatar</label>
-
-        <div class="col-md-7">
-            <input type="file" class="form-control" name="avatar">
+        <div class="control">
+            <input type="file" class="input" name="avatar">
         </div>
 
         @if (!empty($post->featured_image))
@@ -47,68 +42,58 @@
         @endif
     </div>
 
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="bio">Bio</label>
+    <div class="field">
+        <label class="label control-label" for="bio">Bio</label>
 
-        <div class="col-md-7">
-            <textarea class="form-control" name="bio" rows="10">{{ $profile->bio }}</textarea>
-
-            <span class="help-block">
-                Tell us a bit about yourself
-            </span>
+        <div class="control">
+            <textarea class="textarea tiny-mce" name="bio" rows="10">{{ $profile->bio }}</textarea>
         </div>
     </div>
 
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="twitter">Twitter</label>
+    <div class="columns">
+        <div class="column is-one-quarter">
+            <div class="field">
+                <label class="label control-label" for="twitter">Twitter</label>
 
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="twitter" value="{{ $profile->twitter }}">
+                <div class="control">
+                    <input type="text" class="input" name="twitter" value="{{ $profile->twitter }}">
+                </div>
+            </div>
+        </div>
 
-            <span class="help-block">
-                Username only
-            </span>
+        <div class="column is-one-quarter">
+            <div class="field">
+                <label class="label control-label" for="instagram">Instagram</label>
+
+                <div class="control">
+                    <input type="text" class="input" name="instagram"  value="{{ $profile->instagram }}">
+                </div>
+            </div>
+        </div>
+
+        <div class="column is-one-quarter">
+            <div class="field">
+                <label class="label control-label" for="github">GitHub</label>
+
+                <div class="control">
+                    <input type="text" class="input" name="github" value="{{ $profile->github }}">
+                </div>
+            </div>
+        </div>
+
+        <div class="column is-one-quarter">
+            <div class="field">
+                <label class="label control-label" for="dribbble">Dribbble</label>
+
+                <div class="control">
+                    <input type="text" class="input" name="dribbble" value="{{ $profile->dribbble }}">
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="instagram">Instagram</label>
-
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="instagram"  value="{{ $profile->instagram }}">
-
-            <span class="help-block">
-                Username only
-            </span>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="github">GitHub</label>
-
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="github" value="{{ $profile->github }}">
-
-            <span class="help-block">
-                Username only
-            </span>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label class="col-md-3 control-label" for="dribbble">Dribbble</label>
-
-        <div class="col-md-7">
-            <input type="text" class="form-control" name="dribbble" value="{{ $profile->dribbble }}">
-
-            <span class="help-block">
-                Username only
-            </span>
-        </div>
-    </div>
-
-    <div class="col-md-7 col-md-offset-3">
-        <input type="submit" class="btn btn-primary" value="Submit">
+    <div class="control">
+        <input type="submit" class="button is-primary" value="Save Profile">
     </div>
 </form>
 @endsection
