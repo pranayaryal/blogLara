fadb95f74fa27a582a24c57154ec51304c347cd5
Jerrod Long <jlong@doeanderson.com>
2017-12-18 21:04:33 -0500

    [update] Only show social links if they exist

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,12 +7,22 @@
         <h2 class="user_name">{{ $profile->user->name }}</h2>
         <p class="user_title">{{ $profile->title }}</p>
 
+        @if(!empty($profile->instagram) || !empty($profile->twitter) || !empty($profile->github) || !empty($profile->dribbble))
         <ul class="user_social">
+            @if(!empty($profile->instagram))
             <li><a target="_BLANK" href="http://instagram.com/{{ $profile->instagram }}"><i class="fab fa-instagram"></i></a></li>
+            @endif
+            @if(!empty($profile->twitter))
             <li><a target="_BLANK" href="http://twitter.com/{{ $profile->twitter }}"><i class="fab fa-twitter"></i></a></li>
+            @endif
+            @if(!empty($profile->github))
             <li><a target="_BLANK" href="http://github.com/{{ $profile->github }}"><i class="fab fa-github"></i></a></li>
+            @endif
+            @if(!empty($profile->dribbble))
             <li><a target="_BLANK" href="http://dribbble.com/{{ $profile->dribbble }}"><i class="fab fa-dribbble"></i></a></li>
+            @endif
         </ul>
+        @endif
     </div>
 
     <div class="user_bio">
