1fa6d722ae22a9d7ad459c6b20679463556d8016
Zach Hornsby <me@zachhornsby.com>
2017-12-21 11:21:38 -0500

    [fix] logout button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,9 @@
               <hr class="navbar-divider">
               <a class="navbar-item" href="{{ url('/logout') }}"
                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+              </form>
             </div>
           </div>
           @endif
