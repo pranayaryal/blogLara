c68ec5fdf7dcd8f085304ce72eef98062b1fbd84
Zach Hornsby <me@zachhornsby.com>
2018-01-03 15:57:31 -0500

    [update] edit post styling

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -3,130 +3,170 @@
 
 @include('errors')
 
-<a href="/{{ $post->slug }}" class="button" target="_BLANK">View Post</a>
-<br>
-<hr>
-
 <form class="" role="form" action="{{ $action }}" method="POST" enctype="multipart/form-data">
-    <!-- title -->
-    {{ csrf_field() }}
+  <h1 class="page-title">Edit Post</h1>
+  <div class="columns">
+    <div class="column is-two-thirds-tablet">
+      <!-- Title -->
+      {{ csrf_field() }}
 
-    @if (!empty($post->id))
+      @if (!empty($post->id))
         {{ method_field('PUT') }}
         <input class="input" type="hidden" name="id" value="{{ $post->id }}">
-    @endif
-    <input class="input" type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-    
-    {{--  Title  --}}
-    <div class="field">
-        <label class="label control-label">Title</label>
+      @endif
+      <input class="input" type="hidden" name="user_id" value="{{ auth()->user()->id }}">
 
+      <!-- Title -->
+      <div class="field">
         <div class="control">
-            <input class="input" id="create-post-title" type="text" name="title" value="{{ $post->title }}">
+          <input class="input is-large" id="create-post-title" type="text" name="title" value="{{ $post->title }}" placeholder="Title">
         </div>
-    </div>
+      </div>
 
-    <!-- Featured Image -->
-    <div class="field">
-        <label class="label control-label">Featured Image</label>
-
-        <div class="control">
-            <input class="input" id="create-post-title" type="file" name="featured_image" value="{{ $post->featured_image }}">
+      <!-- Content -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-header-title">
+            Content
+          </h3>
         </div>
+        <div class="field">
+          <div class="control">
+            <textarea class="textarea tiny-mce" name="content" rows="15">{{ htmlentities($post->content) }}</textarea>
+          </div>
+        </div>
+      </div>
 
-        @if (!empty($post->featured_image))
-            <img src="{{ $post->featured_image }}">
-        @endif
-    </div>
+      <!-- Excerpt -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-header-title">
+            Excerpt
+          </h3>
+        </div>
+        <div class="field">
+          <div class="control">
+            <textarea class="textarea tiny-mce" name="excerpt" rows="5">{{ htmlentities($post->excerpt) }}</textarea>
+          </div>
+        </div>
+      </div>
 
-    <div class="columns">
-        <!-- Category -->
-        <div class="field column is-one-third">
-            <label class="label control-label">Category</label>
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-header-title">
+            SEO Settings
+          </h3>
+        </div>
+        <div class="card-content">
+          <div class="field">
+            <label class="label control-label">Title</label>
             <div class="control">
-                <div class="select">
-                    <select name="category_id">
-                        @foreach ($categoryOptions as $category)
-                        <option value="{{ $category->id }}" @if (isset($post->category->id) && !empty($post->category->id)) {{ $post->category->id === $category->id ? 'selected' : '' }} @endif>{{ ucfirst($category->name) }}</option>
-                        @endforeach
-                    </select>
-                </div>
+              <input class="input" type="text" name="seo_title" placeholder="CTA | Title | Doe-Anderson" value="{{ !empty(old('seo_title')) ? old('seo_title') : $post->seo_title }}">
             </div>
-        </div>
+          </div>
 
-        <!-- Status -->
-        <div class="field column is-one-third">
+          <div class="field">
+            <label class="label control-label">Description</label>
+            <div class="control">
+              <textarea class="textarea" type="text" name="seo_description">{{ !empty(old('seo_description')) ? old('seo_description') : $post->seo_description }}</textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <!-- Create / Save Post -->
+      <div class="field">
+        <div class="control">
+          <input class="button is-primary is-medium is-fullwidth" type="submit" value="{{ $action === '/post' ? 'Create' : 'Update' }} ">
+        </div>
+      </div>
+
+      <!-- View Post -->
+      <div class="field">
+        <div class="control">
+          <a href="/{{ $post->slug }}" class="button is-fullwidth" target="_BLANK">View Post</a>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-header-title">
+            Settings
+          </h3>
+        </div>
+        <div class="card-content">
+          <!-- Status -->
+          <div class="field">
             <label class="label control-label">Status</label>
             <div class="control">
-                <div class="select">
-                    <select name="status_id">
-                        @foreach ($statusOptions as $status)
-                        <option value="{{ $status->id }}" @if (isset($post->status->id) && !empty($post->status->id)){{ $post->status->id === $status->id ? 'selected' : '' }} @endif>{{ ucfirst($status->name) }}</option>
-                        @endforeach
-                    </select>
-                </div>
+              <div class="select is-fullwidth">
+                <select name="status_id">
+                  @foreach ($statusOptions as $status)
+                  <option value="{{ $status->id }}" @if (isset($post->status->id) && !empty($post->status->id)){{ $post->status->id === $status->id ? 'selected' : '' }} @endif>{{ ucfirst($status->name) }}</option>
+                  @endforeach
+                </select>
+              </div>
             </div>
-        </div>
+          </div>
 
-        <!-- Canonical -->
-        <div class="field column is-one-third">
-            <label class="label control-label">Canonical link</label>
+          <!-- Canonical -->
+          <div class="field">
             <div class="control">
+              <label class="checkbox">
                 <input class="checkbox" name="canonical" type="checkbox" {{ $post->canonical === 1 ? 'checked' : '' }}>
+                Canonical link
+              </label>
             </div>
-        </div>
-    </div>
+          </div>
 
-    <div class="columns">
-        <div class="field column is-one-half">
-            <label class="label control-label">SEO Title</label>
+          <!-- Category -->
+          <div class="field">
+            <label class="label control-label">Category</label>
             <div class="control">
-                <input class="input" type="text" name="seo_title" placeholder="CTA | Title | Doe-Anderson" value="{{ !empty(old('seo_title')) ? old('seo_title') : $post->seo_title }}">
+              <div class="select is-fullwidth">
+                <select name="category_id">
+                  @foreach ($categoryOptions as $category)
+                  <option value="{{ $category->id }}" @if (isset($post->category->id) && !empty($post->category->id)) {{ $post->category->id === $category->id ? 'selected' : '' }} @endif>{{ ucfirst($category->name) }}</option>
+                  @endforeach
+                </select>
+              </div>
             </div>
-        </div>
+          </div>
 
-        <div class="field column is-one-half">
-            <label class="label control-label">SEO Description</label>
+          <!-- Featured Image -->
+          <div class="field">
+            <label class="label control-label">Featured Image</label>
+
             <div class="control">
-                <textarea class="textarea" type="text" name="seo_description">{{ !empty(old('seo_description')) ? old('seo_description') : $post->seo_description }}</textarea>
+              <input class="input" id="create-post-title" type="file" name="featured_image" value="{{ $post->featured_image }}">
             </div>
+
+            @if (!empty($post->featured_image))
+              <img src="{{ $post->featured_image }}">
+            @endif
+          </div>
         </div>
-    </div>
+      </div>
 
-    <!-- post content -->
-    <div class="field">
-        <label class="label control-label">Content</label>
-
+      <div class="field">
         <div class="control">
-            <textarea class="textarea tiny-mce" name="content" rows="15">{{ htmlentities($post->content) }}</textarea>
+          <a href="/posts/{{ $post->id }}/delete"
+            onclick="event.preventDefault();
+            document.getElementById('delete-form-{{ $post->id }}').submit();"
+            class="button is-danger">
+            Delete Post
+          </a>
         </div>
+      </div>
     </div>
-
-    <!-- post excerpt -->
-    <div class="field">
-        <label class="label control-label">Excerpt</label>
-
-        <div class="control">
-            <textarea class="textarea tiny-mce" name="excerpt" rows="15">{{ htmlentities($post->excerpt) }}</textarea>
-        </div>
-    </div>
-
-    
-    <div class="buttons">
-        <input class="button is-primary" type="submit" value="{{ $action === '/post' ? 'Create' : 'Edit' }} ">
-
-        <a href="/posts/{{ $post->id }}/delete"      
-            onclick="event.preventDefault();     
-            document.getElementById('delete-form-{{ $post->id }}').submit();"     
-            class="button is-danger">       
-            Delete        
-        </a>
-    </div>
+  </div>
 </form>
 
-<form id="delete-form-{{ $post->id }}" action="/posts/{{ $post->id }}/delete" method="POST" style="display: none;">       
-    {{ method_field('DELETE') }}      
-    {{ csrf_field() }}        
+<form id="delete-form-{{ $post->id }}" action="/posts/{{ $post->id }}/delete" method="POST" style="display: none;">
+  {{ method_field('DELETE') }}
+  {{ csrf_field() }}
 </form>
 @endsection
 @include('tinymce');
