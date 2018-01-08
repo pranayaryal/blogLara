5c1f5fb9256c48a7601604d2be7d8b87a49ccb54
Jerrod Long <jlong@doeanderson.com>
2018-01-08 12:50:16 -0500

    [add] Link featured image to post

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -1,25 +1,27 @@
 <article class="post">
   <header class="post_header">
-      <h2 class="post_title"><a href="{{ $post->slug }}">{{ $post->title }}</a></h2>
+    <h2 class="post_title"><a href="{{ $post->slug }}">{{ $post->title }}</a></h2>
 
-      <div class="post_meta">
-        @if (!empty($post->author))
-        <div class="author_avatar" style="background-image:url('{{ $post->author->profile->avatar }}');"></div>
-        <div class="post_details"><a class="author_name" href="/profile/{{ $post->author->profile->slug }}" title="{{ $post->author->name }}">{{ $post->author->name }}</a> in <a class="category_name" href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></div>
-        @endif
-        <time class="post_date">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</time>
-        {{-- @if (!empty($post->updated_at))
-        <time class="post_date edit_date"> | Edited: {{ \Carbon\Carbon::parse($post->updated_at)->diffForHumans() }}</time>
-        @endif --}}
-      </div>
+    <div class="post_meta">
+      @if (!empty($post->author))
+      <div class="author_avatar" style="background-image:url('{{ $post->author->profile->avatar }}');"></div>
+      <div class="post_details"><a class="author_name" href="/profile/{{ $post->author->profile->slug }}" title="{{ $post->author->name }}">{{ $post->author->name }}</a> in <a class="category_name" href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></div>
+      @endif
+      <time class="post_date">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</time>
+      {{-- @if (!empty($post->updated_at))
+      <time class="post_date edit_date"> | Edited: {{ \Carbon\Carbon::parse($post->updated_at)->diffForHumans() }}</time>
+      @endif --}}
+    </div>
   </header>
 
   @if(!empty($post->featured_image))
   <div class="post_image">
-    <picture>
-      <source srcset="{{ $post->featured_image }}" media="(min-width: 1280px)">
-      <img srcset="{{ $post->featured_image }}" alt="">
-    </picture>
+    <a href="{{ request()->root() . '/' . $post->slug }}">
+      <picture>
+        <source srcset="{{ $post->featured_image }}" media="(min-width: 1280px)">
+        <img srcset="{{ $post->featured_image }}" alt="">
+      </picture>
+    </a>
   </div>
   @endif
 
