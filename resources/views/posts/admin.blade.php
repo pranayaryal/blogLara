25e148e94a6099f80d0bb69bdc0c4f247939cac6
Jerrod Long <jlong@doeanderson.com>
2018-01-22 21:15:51 -0500

    [add] Finalized views. Add basic styling for adding profile edit to admin

--- resources/views/posts/admin.blade.php
+++ resources/views/posts/admin.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="column">
-    <h2>Edit Posts</h2>
+<div class="container columns admin">
+  <div class="column is-two-thirds posts">
+    <h2 class="title">Edit Posts</h2>
     @each('posts.listItem', $posts, 'post', 'posts.empty')
   </div>
 
-  <aside class="column">
-    <h2>Edit Profiles</h2>
+  <aside class="column is-one-thirds profiles">
+    <h2 class="title">Edit Profiles</h2>
     @each('profile.list', $profiles, 'profile', 'profile.empty')
   </aside>
 </div>
--- resources/views/profile/list.blade.php
+++ resources/views/profile/list.blade.php
@@ -1,1 +1,31 @@
-List profiles
+<article class="post post--list">
+  <header class="post_header">
+    <h2 class="post_title">
+      <div class="post_meta">
+        @if(!empty($profile->avatar))
+        <div class="author_avatar" style="background-image:url('{{ $profile->avatar }}')"></div>
+        @else
+        <div class="author_avatar"></div>
+        @endif
+      </div>
+      <a href="{{ request()->root() . '/' . $profile->slug }}">{{ $profile->user->name }}</a>
+    </h2>
+  </header>
+
+  @if(Auth::check())
+  <div class="post_control">
+    <a class="button is-small" href="/profiles/{{ $profile->id }}/edit">Edit</a>
+    <a href="/profiles/{{ $profile->id }}/delete"
+        onclick="event.preventDefault();
+        document.getElementById('delete-form-{{ $profile->id }}').submit();"
+        class="button is-danger is-small">
+        Delete
+    </a>
+    <form id="delete-form-{{ $profile->id }}" action="/profiles/{{ $profile->id }}/delete" method="POST" style="display: none;">
+      {{ method_field('DELETE') }}
+      {{ csrf_field() }}
+    </form>
+  </div>
+  @endif
+</article>
+  
